44d68566694451c70b3d9b9e828200d40ac77546
suvarnalakshmiarumalla <suvarnalakshmiarumalla@gmail.com>
2024-10-24 13:50:16 +0530

    message1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/UseContext_ref/", // Ensure the base path is correct
+    base: "react_classwork_d", // Ensure the base path is correct
     plugins: [react()],
 });
